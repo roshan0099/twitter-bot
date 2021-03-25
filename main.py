59b998364c5b43121e1919db636242de84ad76f0
roshan0099 <roshan09ka@gmail.com>
2021-03-25 11:38:27 +0530

    changed a bit

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from time import sleep
 from name_fetch import Human
 import os
 
+
+global id_maintainer
 # function to take in last seen id and also to return it 
 def last_seen(FILE,id = 0) :
 	if id != 0 :
 		f = open(FILE,"w")
 		f.write(id)
+		id_maintainer = id
+		print(type(id_maintainer))
 	else :
 		f = open(FILE,"r")
-		return f.read()
+		return int(f.read())
 
 #function to extract the name of the artist from the sentence 
 def extract_name(text,limit):
@@ -55,56 +59,56 @@ def main_core_loop(mention_info,FILE,api,fs) :
 	for index,info in enumerate(mention_info) :
 		tweet = info.full_text.lower()
 	
-		try :
-			if index == 0 :
-				last_seen(FILE,info.id_str)
+		# try :
+		if index == 0 :
+			last_seen(FILE,info.id_str)
 
-			if "#beta" in  tweet :
+		if "#beta" in  tweet :
 
-				#condition to check if asked is for recommendation or not 
-				if "recommendations" in tweet or "recommend" in tweet or "suggest" in tweet :
-					recommended_tracks = recommendations()
-					api.update_status(status = "I have found these for you 🕵 : "+", ".join(recommended_tracks),in_reply_to_status_id = info.id,auto_populate_reply_metadata=True) 
-				
-				#if not it assumes what asked for is top ten tracks
-				elif "from" in tweet:
-					album_name = extract_name(tweet,'from')
-					tracks = tracks_from_album(album_name)
-					api.update_status(status = "here are some of em 🕺 : "+", ".join(tracks),in_reply_to_status_id = info.id,auto_populate_reply_metadata=True)
-				else :	
-					artist = extract_name(tweet,'of')
-				
+			#condition to check if asked is for recommendation or not 
+			if "recommendations" in tweet or "recommend" in tweet or "suggest" in tweet :
+				recommended_tracks = recommendations()
+				api.update_status(status = "I have found these for you 🕵 : "+", ".join(recommended_tracks),in_reply_to_status_id = info.id,auto_populate_reply_metadata=True) 
+			
+			#if not it assumes what asked for is top ten tracks
+			elif "from" in tweet:
+				album_name = extract_name(tweet,'from')
+				tracks = tracks_from_album(album_name)
+				api.update_status(status = "here are some of em 🕺 : "+", ".join(tracks),in_reply_to_status_id = info.id,auto_populate_reply_metadata=True)
+			else :	
+				artist = extract_name(tweet,'of')
+			
 
-					tracks = spotify.getting_top_tracks(" ".join(artist))
+				tracks = spotify.getting_top_tracks(" ".join(artist))
 
-					api.update_status(status ="Here are the top 5 tracks : "+ ", ".join(tracks),in_reply_to_status_id = info.id,auto_populate_reply_metadata=True) 	
-					
-
-
-			elif "#info" in tweet	:
-				
-				reply = """hey 🙋‍♂️ \nThe bot works with  
-											keywords and hastags. You want recommendations ? use the keyword recommend/suggest/recommendations with
-											#beta at the end (dont forget the hash tag 😿) \n\n
-											eg : yo, 'recommend'some tracks #beta => remember 'recommend' and '#beta'😏"""
-
-										
-				api.update_status(status = reply ,in_reply_to_status_id = info.id,auto_populate_reply_metadata=True)
+				api.update_status(status ="Here are the top 5 tracks : "+ ", ".join(tracks),in_reply_to_status_id = info.id,auto_populate_reply_metadata=True) 	
 				
 
 
+		elif "#info" in tweet	:
+			
+			reply = """hey 🙋‍♂️ \nThe bot works with  
+										keywords and hastags. You want recommendations ? use the keyword recommend/suggest/recommendations with
+										#beta at the end (dont forget the hash tag 😿) \n\n
+										eg : yo, 'recommend'some tracks #beta => remember 'recommend' and '#beta'😏"""
 
-			else :
-				#to give out random text based on the tweet
-				#like hai, whats your name or simple just oops, error or something
-				response = Human()
-				text = response.response_text(tweet)
-				api.update_status(status = text,in_reply_to_status_id = info.id,auto_populate_reply_metadata=True) 
+									
+			api.update_status(status = reply ,in_reply_to_status_id = info.id,auto_populate_reply_metadata=True)
+			
+
+
+
+		else :
+			#to give out random text based on the tweet
+			#like hai, whats your name or simple just oops, error or something
+			response = Human()
+			text = response.response_text(tweet)
+			api.update_status(status = text,in_reply_to_status_id = info.id_str,auto_populate_reply_metadata=True) 
 
 
 			
-		except :
-			api.update_status(status = "oops an excpetion has occured, will get back to you soon",in_reply_to_status_id = info.id,auto_populate_reply_metadata=True) 
+		# except :
+		# 	api.update_status(status = "oops an excpetion has occured, will get back to you soon",in_reply_to_status_id = info.id,auto_populate_reply_metadata=True) 
 	fs.close()
 
 
@@ -134,6 +138,9 @@ def main() :
 	while True :
 		
 		mention_info = api.mentions_timeline(last_seen(FILE),tweet_mode = "extended")
+		# print(mention_info)
+		# for index,info in enumerate(mention_info) :
+		# 	print(info.id_str)
 		main_core_loop(mention_info,FILE,api,fs)
 		sleep(40)
 
