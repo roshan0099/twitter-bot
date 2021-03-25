94cc1eed301998d7b2f0b273aa5319a197d4b934
roshan0099 <roshan09ka@gmail.com>
2021-03-26 03:23:02 +0530

    added things

--- fetch_id.py
+++ fetch_id.py
@@ -7,25 +7,27 @@ import os
 class Fetch_id :
 
 	def fetch(self,id_vid):
+		try :
+			chrome_options = webdriver.ChromeOptions()
 
-		chrome_options = webdriver.ChromeOptions()
+			chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+			chrome_options.add_argument["--headless"]
+			chrome_options.add_argument["--disable-dev-shm-usage"]
+			chrome_options.add_argument["--no-sandbox"]
+			driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVE PATH'), chrome_options=chrome_optionse)
 
-		chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-		chrome_options.add_argument["--headless"]
-		chrome_options.add_argument["--disable-dev-shm-usage"]
-		chrome_options.add_argument["--no-sandbox"]
-		driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVE PATH'), chrome_options=chrome_optionse)
+			url = f"https://ytdlink.herokuapp.com/{id_vid}"
 
-		url = f"https://ytdlink.herokuapp.com/{id_vid}"
+			# PATH = "C:\Program Files (x86)\chromedriver.exe"
 
-		# PATH = "C:\Program Files (x86)\chromedriver.exe"
+			# driver = webdriver.Chrome(PATH)
 
-		# driver = webdriver.Chrome(PATH)
+			driver.get(url)  
 
-		driver.get(url)  
+			time.sleep(10)
 
-		time.sleep(10)
-
-		soup = BeautifulSoup(driver.page_source,"html.parser")
-		print(soup)
-		return soup.body.div.string
+			soup = BeautifulSoup(driver.page_source,"html.parser")
+			print(soup)
+			return soup.body.div.string
+		except :
+			return(["ooops an exception has occured"])	
